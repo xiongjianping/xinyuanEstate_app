f966f04e5eeb0a950115f427523979c138219dc7
fanlongfei <fanlongfei@maxrocky.com>
2018-08-04 00:51:00 +0800

    添加生产环境地址

--- src/components/js/comaxios.js
+++ src/components/js/comaxios.js
@@ -4,8 +4,9 @@ import axios from 'axios'
 var isRefresh = true
 
 
-axios.defaults.baseURL = "http://10.17.9.123:8080" // 本地
-    // axios.defaults.baseURL = "http://192.168.3.33:8080" // 测试
+// axios.defaults.baseURL = "http://10.17.9.123:8080" // 本地
+// axios.defaults.baseURL = "http://192.168.3.33:8080" // 测试
+axios.defaults.baseURL = "http://192.168.0.56:8080" // 生产
 axios.defaults.withCredentials = true
 axios.defaults.timeout = 10000
 
